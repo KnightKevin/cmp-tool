edb5a6fdddd938c00675b5cb67bf5e0f42ea42da
qiao.guo <tt@tt.com>
2024-01-30 16:26:40 +0800

    long job

--- git-push-all.sh
+++ git-push-all.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+ignoreDir=("logs/" "cloud/")
+
 # 进入包含所有仓库的文件夹
 cd ./
 
 # 遍历所有文件夹（每个文件夹都是一个 Git 仓库）
 for repo in */; do
-    if [ "$repo" == "logs/" ]; then
+    if [[ " ${ignoreDir[*]} " == *" $repo "* ]]; then
           echo "Skipping $repo..."
           continue
     fi
