a93a6cda087917a74f4ca35a9292e54d283c1138
qiao.guo <tt@tt.com>
2024-03-28 09:51:28 +0800

    国际化

--- cloud-manager/src/test/java/com/zscmp/main/test/I18nTest.java
+++ cloud-manager/src/test/java/com/zscmp/main/test/I18nTest.java
@@ -5,12 +5,17 @@ package com.zscmp.main.test;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MemberValuePair;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
 import com.github.javaparser.ast.nodeTypes.modifiers.NodeWithPublicModifier;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.zscmp.cloud.manager.JavaFileVisitor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -25,12 +30,11 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -211,11 +215,98 @@ public class I18nTest {
     }
 
     @Test
-    public void dd() {
-        Locale locale = Locale.forLanguageTag("zh-CN");
+    public void stringLiteralI18nTest() {
+        Path startPath = Paths.get("D:\\work_space\\cmp-tool\\maintenance-server"); // 指定你要遍历的目录路径
+        JavaFileVisitor visitor = new JavaFileVisitor();
+
+        try {
+            Files.walkFileTree(startPath, visitor);
+            List<Path> javaFiles = visitor.getJavaFiles();
+
+            for (Path javaFile : javaFiles) {
+
+                String javaClassName = javaFile.getFileName().toString().replace(".java", "");
+
+                if (!javaFile.toUri().toString().endsWith(".java")) {
+                    continue;
+                }
+
+                CompilationUnit cu = StaticJavaParser.parse(javaFile);
+
+                cu.accept(new VoidVisitorAdapter<Void>() {
+                    @Override
+                    public void visit(StringLiteralExpr n, Void arg) {
+                        super.visit(n, arg);
+
+                        if (n.getValue().contains("metricData.cpu_util")) {
+                            log.info("已使用, file {} at line {}", javaClassName,n.getRange().map(r -> r.begin.line).orElse(-1));
+                        }
+
+                    }
+                }, null);
+
+            }
 
 
-        assertEquals(locale, Locale.SIMPLIFIED_CHINESE);
+        } catch (IOException e) {
+            System.err.println("Error walking through the directory: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void functionUseInfo() {
+        Path startPath = Paths.get("D:\\work_space\\cmp-tool\\maintenance-server"); // 指定你要遍历的目录路径
+        JavaFileVisitor visitor = new JavaFileVisitor();
+
+        final String targetMethodCall = "MessageSourceHelper#getMessage";
+
+        try {
+            Files.walkFileTree(startPath, visitor);
+            List<Path> javaFiles = visitor.getJavaFiles();
+
+            for (Path javaFile : javaFiles) {
+
+                String javaClassName = javaFile.getFileName().toString().replace(".java", "");
+
+                if (!javaFile.toUri().toString().endsWith(".java")) {
+                    continue;
+                }
+
+                CompilationUnit cu = StaticJavaParser.parse(javaFile);
+
+                cu.accept(new VoidVisitorAdapter<Void>() {
+                    @Override
+                    public void visit(MethodCallExpr method, Void arg) {
+                        super.visit(method, arg);
+
+                        Expression scope = method.getScope().orElse(null);
+                        String methCall = String.format("%s#%s", scope==null?"":scope.toString(),method.getName().getIdentifier());
+                        if (methCall.equalsIgnoreCase(targetMethodCall)) {
+
+                            Expression argExpr = method.getArguments().get(0);
+                            if (!argExpr.isFieldAccessExpr() || !argExpr.asFieldAccessExpr().toString().startsWith("ConstI18nKey.")) {
+
+                                List<String> dd = method.getArguments().stream().map(Node::toString).collect(Collectors.toList());
+
+                                log.info("不合法的 {}({}) {} at line {}",methCall, String.join(",", dd), javaClassName, method.getRange().map(r->r.begin.line).orElse(-1));
+                            }
+
+                        }
+
+                    }
+                }, null);
+
+            }
+
+
+        } catch (IOException e) {
+            System.err.println("Error walking through the directory: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void i18nFile() {
+//        loadPropertiesFile("")
     }
 
     /**
@@ -290,5 +381,14 @@ public class I18nTest {
         }
 
         return "";
+    }
+
+    public Properties loadPropertiesFile(String filename) throws IOException {
+        Properties properties = new Properties();
+        // 使用ClassLoader获取资源的路径，以便从相对于classpath的位置加载
+        String path = getClass().getClassLoader().getResource(filename).getPath();
+        // 加载文件
+        properties.load(Files.newInputStream(Paths.get(path)));
+        return properties;
     }
 }
