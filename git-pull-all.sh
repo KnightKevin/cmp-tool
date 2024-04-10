69af3fd835dc7de2c9fef669317d38a4c7d5b665
qiao.guo <tt@tt.com>
2024-04-10 14:30:42 +0800

    http://jira.zstack.io/browse/CMP-13062

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -5,7 +5,7 @@ cd ./
 
 # 遍历所有文件夹（每个文件夹都是一个 Git 仓库）
 for repo in */; do
-      if [ "$repo" == "logs/" ]; then
+      if [ "$repo" == "logs/" ] || [ "$repo" == "cloud-manager/" ]; then
             echo "Skipping $repo..."
             continue
       fi
