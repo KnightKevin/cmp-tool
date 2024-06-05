cd8b37ec29eebaccbe4767827334e08f0d0aece4
simon <tt@dd.com>
2024-06-05 06:01:58 +0000

    sh

--- git-update-index.sh
+++ git-update-index.sh
@@ -3,17 +3,27 @@
 # 用于忽略本地开发环境中配置文件的修改，效果就是修改了配置文件，执行git status时不会出现在待提交中（注意区别直接忽略）
 
 # 获取当前目录
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dirs=$(ls -l | grep '^d' | awk '{print $9}')
 
 
 # 列出当前目录下的所有文件夹
-for folder in "$DIR"/*; do
-    if [ -d "$folder" ]; then
-        cd "$folder" || exit
+for folder in $dirs; do
+    # 检查文件夹是否是一个Git项目
+    cd $folder
 
-        # 检查文件夹是否是一个Git项目
-        if [ -d ".git" ]; then
+    if [ -d ".git" ]; then
+        case $folder in
+        "basic-libs")
+        # skip
+        ;;
+        "gateway-server")
+            git update-index --assume-unchanged src/main/resources/application-local.yml
+        ;;
+        *)
             git update-index --assume-unchanged starter/src/main/resources/application-local.yml
-        fi
+        ;;
+        esac 
+
     fi
+    cd ..
 done
